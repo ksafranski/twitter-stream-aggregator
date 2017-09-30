3dd78532270317d4dccb30668fd1331ad1e7b686
fluidbyte <kent@fluidbyte.net>
2017-09-30 15:48:15 -0500

    feat(stream): data from tweet stream loading in UI

--- client/lib/components/Stream.js
+++ client/lib/components/Stream.js
@@ -6,17 +6,44 @@ const io = window.io
 export default class Stream extends React.Component {
   constructor (props, ctx) {
     super(props)
-
+    
+    // Initial state
+    this.state = {
+      tweets: []
+    }
+    
+    // Create socket connection
     const socket = io.connect()
-
+    
+    // Save from overloading the DOM
+    const maxTweets = 30
+    
+    // Handle events
     socket.on('twitter', (data) => {
-      console.log(data)
+      let curTweets = this.state.tweets
+      
+      // Continue to slice to prevent overload
+      curTweets = curTweets.length > maxTweets 
+        ? curTweets.slice(curTweets.length - maxTweets)
+        : curTweets
+      
+      // Push new tweet
+      curTweets.push(data.tweet)
+      
+      // Set state
+      this.setState({
+        tweets: curTweets
+      })
     })
   }
 
   render () {
     return (
-      <div>Stream...</div>
+      <ul className='tweet-stream'>
+        {this.state.tweets.map((t, i) => (
+          <li key={i++}>{t}</li>
+        ))}
+      </ul>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,19 +4,26 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const twitter = require('./lib/twitter')
+const EventEmitter = require('events')
+
+// Create emitter for twitter stream
+class TwitterEmitter extends EventEmitter {}
+const tweetStream = new TwitterEmitter()
 
 const PORT = process.env.PORT || 8080
 const CLIENT_PATH = path.resolve(__dirname, '../client')
 
+// Start twitter stream
 twitter.stream({
   filters: [
     'emojis',
     'hashtags',
     'urls'
   ],
+  // Flip to `true` to see stream in console
   debug: false
 }).on('data', (data) => {
-  return null
+  tweetStream.emit('data', data)
 })
 
 // Listen on PORT
@@ -27,7 +34,10 @@ app.use(express.static(CLIENT_PATH))
 
 // Establish socket connections
 io.on('connection', (socket) => {
-  socket.emit('twitter', { hello: 'world' })
+  // On twitter events, emit data
+  tweetStream.on('data', (data) => {
+    socket.emit('twitter', data)
+  })
 })
 
 // Log out to indicate server start
