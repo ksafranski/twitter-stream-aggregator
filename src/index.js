e214ca72e25c141b9a2f3bb95405aca16d530a5d
fluidbyte <kent@fluidbyte.net>
2017-09-30 11:36:50 -0500

    feat(client): setting up initial client app and service

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-const twitter = require('./lib/twitter')
+const path = require('path')
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+// const io = require('socket.io')(server)
+// const twitter = require('./lib/twitter')
 
-twitter.stream({
-  filters: [
-    'emojis',
-    'hashtags',
-    'urls'
-  ],
-  debug: true
-})
+const PORT = process.env.PORT || 8080
+const CLIENT_PATH = path.resolve(__dirname, '../client')
+
+// twitter.stream({
+//   filters: [
+//     'emojis',
+//     'hashtags',
+//     'urls'
+//   ],
+//   debug: true
+// })
+
+// Listen on PORT
+server.listen(PORT)
+
+// Serve static (client app)
+app.use(express.static(CLIENT_PATH))
+
+// Log out to indicate server start
+console.log(`Service running over ${PORT}`)
