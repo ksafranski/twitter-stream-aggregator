bc525fe2e6ddccd7a08d02a01c96f88fd251a1d9
fluidbyte <kent@fluidbyte.net>
2017-09-30 15:49:00 -0500

    fix(lint): misc lint fixes

--- client/lib/components/Stream.js
+++ client/lib/components/Stream.js
@@ -6,30 +6,30 @@ const io = window.io
 export default class Stream extends React.Component {
   constructor (props, ctx) {
     super(props)
-    
+
     // Initial state
     this.state = {
       tweets: []
     }
-    
+
     // Create socket connection
     const socket = io.connect()
-    
+
     // Save from overloading the DOM
     const maxTweets = 30
-    
+
     // Handle events
     socket.on('twitter', (data) => {
       let curTweets = this.state.tweets
-      
+
       // Continue to slice to prevent overload
-      curTweets = curTweets.length > maxTweets 
+      curTweets = curTweets.length > maxTweets
         ? curTweets.slice(curTweets.length - maxTweets)
         : curTweets
-      
+
       // Push new tweet
       curTweets.push(data.tweet)
-      
+
       // Set state
       this.setState({
         tweets: curTweets
