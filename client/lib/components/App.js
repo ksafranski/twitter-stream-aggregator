e099207dcae167493adaef917228e22ec79b81b7
fluidbyte <kent@fluidbyte.net>
2017-09-30 12:32:35 -0500

    feat(ui): adding base ui components and css

--- client/lib/components/App.js
+++ client/lib/components/App.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default class App extends React.Component {
   render () {
     return (
-      <h1>Hello World!</h1>
+      <div class="container">
+        <h1>Hello World!</h1>
+      </div>
     )
   }
 }
--- client/lib/main.js
+++ client/lib/main.js
@@ -1,3 +1,4 @@
+/* eslint no-unused-vars: 0 */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
